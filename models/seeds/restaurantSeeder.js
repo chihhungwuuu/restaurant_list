a3ce61dccf0638eb340f9987c1f43f26e658a69e
Allen <allenwu032016@gmail.com>
2023-11-26 16:10:58 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const Restaurant = require("../restaurant") // 載入 todo model
+
+const restaurantList = require("../../restaurant.json")
+let restaurants = restaurantList.results
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on("error", () => {
+  console.log("mongodb error!")
+})
+db.once("open", () => {
+  console.log("mongodb connected!")
+
+  for (let i = 0; i < restaurants.length; i++) {
+    Restaurant.create({
+      id: restaurants[i].id,
+      name: restaurants[i].name,
+      name_en: restaurants[i].name_en,
+      category: restaurants[i].category,
+      image: restaurants[i].image,
+      location: restaurants[i].location,
+      phone: restaurants[i].phone,
+      google_map: restaurants[i].google_map,
+      rating: restaurants[i].rating,
+      description: restaurants[i].description
+    })
+  }
+
+  console.log("done")
+})
