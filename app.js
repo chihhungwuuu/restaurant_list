60666da6e8016090204f1d1d3e7f34d0ca78daf8
Allen <allenwu032016@gmail.com>
2023-11-28 21:47:38 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -120,6 +120,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+// 刪除餐廳
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 // 搜尋餐廳
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
