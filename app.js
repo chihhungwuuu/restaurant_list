2b980152b835a0ad7530eb8b132e70ac762d8482
Allen <allenwu032016@gmail.com>
2023-11-28 21:37:05 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const Restaurant = require("./models/restaurant")
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -15,38 +15,38 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const db = mongoose.connection
 
-db.on("error", () => {
-  console.log("mongodb error!")
+db.on('error', () => {
+  console.log('mongodb error!')
 })
 
-db.once("open", () => {
-  console.log("mongodb connected!")
+db.once('open', () => {
+  console.log('mongodb connected!')
 })
 
 const app = express()
 const port = 3000
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 // 瀏覽全部餐廳
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
-    .then((restaurants) => res.render("index", { restaurants }))
+    .then((restaurants) => res.render('index', { restaurants }))
     .catch((error) => console.error(error))
 })
 
-app.get("/restaurants/new", (req, res) => {
-  return res.render("new")
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
 })
 
 //新增餐廳
-app.post("/restaurants", (req, res) => {
+app.post('/restaurants', (req, res) => {
   const name = req.body.name
   const name_en = req.body.name_en
   const category = req.body.category
@@ -68,25 +68,65 @@ app.post("/restaurants", (req, res) => {
     rating,
     description
   })
-    .then(() => res.redirect("/"))
+    .then(() => res.redirect('/'))
     .catch((error) => console.log(error))
 })
 
 //瀏覽特定餐廳
-app.get("/restaurants/:id", (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(req.params.id)
     .lean()
-    .then((restaurant) => res.render("show", { restaurant }))
+    .then((restaurant) => res.render('show', { restaurant }))
     .catch((error) => console.log(error))
 })
 
-app.get("/search", (req, res) => {
+// 前往edit頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+//修改特定餐廳
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch((error) => console.log(error))
+})
+
+// 搜尋餐廳
+app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter((restaurant) => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
-  res.render("index", { restaurants: restaurants, keyword: keyword })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
